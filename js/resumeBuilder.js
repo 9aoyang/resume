89340655ee8ee198c4f89a93b3e92c88b2ccda6a
9aoyang <9aoyang@gmail.com>
2017-09-04 21:47:59 +0800

    add name && role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// try jQuery
+// $('#main').append('9aoyang');
+
+
+
+// first var
+// var awesomeThoughts = 'i am 9aoyang and i am awesome';
+// console.log(awesomeThoughts);
+// var funThoughts = awesomeThoughts.replace('awesome', 'fun');
+// console.log(funThoughts);
+// $('#main').append(funThoughts);
+
+var formattedName = HTMLheaderName.replace('%data%', '羔羊');
+var formattedRole = HTMLheaderRole.replace('%data%', 'Front-end');
+$('#header').append(formattedName);
+$('#header').append(formattedRole);
