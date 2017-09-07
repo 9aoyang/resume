52be730d20812a16b0b36ca7073cdd1071908af2
9aoyang <9aoyang@gmail.com>
2017-09-07 20:48:08 +0800

    buliding page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,7 +10,14 @@
 // console.log(funThoughts);
 // $('#main').append(funThoughts);
 
-var formattedName = HTMLheaderName.replace('%data%', '羔羊');
-var formattedRole = HTMLheaderRole.replace('%data%', 'Front-end');
-$('#header').append(formattedName);
-$('#header').append(formattedRole);
+var formattedName = HTMLheaderName.replace('%data%', '9aoyang');
+var role = 'Front-end';
+var formattedRole = HTMLheaderRole.replace('%data%', role);
+// append会添加到<ul></ul>之后，为了添加元素到开头，应使用prepend
+// $('#header').append(formattedName);
+// $('#header').append(formattedRole);
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
+
+var skills = ["HTML", "CSS", "JavaScript"];
+$('#main').append(skills[1]);
