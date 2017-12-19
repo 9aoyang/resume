967f1d6d5a6fe1a97fa426a68010ecab84c072ce
9aoyang <9aoyang@gmail.com>
2017-12-19 22:42:54 +0800

    :construction: finish work info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -19,9 +19,16 @@ let work = {
         {
             'Employer': 'DXY',
             'Title': 'Front End Development',
-            'Date': '2017.10-2018.1',
+            'Dates': '2017.10 - 2018.1',
             'Location': 'Hangzhou, Zhejiang,',
-            // 'Description': ''
+            'Description': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, delectus laborum repellat eos magnam optio libero sapiente laboriosam velit expedita iste amet odit totam voluptate ratione adipisci illo quaerat sequi?'
+        },
+        {
+            'Employer': 'doBell',
+            'Title': 'Front End Development',
+            'Dates': '2017.6 - 2018.9',
+            'Location': 'Hefei, Anhui,',
+            'Description': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, delectus laborum repellat eos magnam optio libero sapiente laboriosam velit expedita iste amet odit totam voluptate ratione adipisci illo quaerat sequi?'
         }
     ]
 };
@@ -40,11 +47,27 @@ if (bio.skills.length > 0) {
     }
 }
 
-for (job in work.jobs) {
-    $('#workExperience').append(HTMLworkStart)
-    let formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].Employer);
-    let formattedTitle = HTMLworkTitle.replace('%data%', work.jobs[job].Title)
-    let formattedEmployerTitle = formattedEmployer + formattedTitle
-    $('.work-entry:last').append(formattedEmployerTitle);
+var displayWork = function () {
+    for (job in work.jobs) {
+        $('#workExperience').append(HTMLworkStart)
+        let formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].Employer);
+        let formattedTitle = HTMLworkTitle.replace('%data%', work.jobs[job].Title)
+        let formattedEmployerTitle = formattedEmployer + formattedTitle
+        $('.work-entry:last').append(formattedEmployerTitle);
+        let formattedDates = HTMLworkDates.replace('%data%', work.jobs[job].Dates)
+        $('.work-entry:last').append(formattedDates);
+        let formattedDescription = HTMLworkDescription.replace('%data%', work.jobs[job].Description)
+        $('.work-entry:last').append(formattedDescription);
+    }
 }
+
+displayWork();
+
+$(document).click(function(loc) {
+    let x = loc.pageX;
+    let y = loc.pageY;
+    logClicks(x, y);
+})
+
+
 
